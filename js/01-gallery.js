8d75b687aead32a2fcc01ba0ef515850b15b8712
Volodymyr Babych <v.d.babych@gmail.com>
2022-11-08 21:45:00 +0200

    add new way

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,84 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryMarkup = createMurkupFromObjArr(galleryItems)
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryMarkup;
+
+galleryEl.addEventListener('click', onGalleryItemClick);
+
+
+
+function onGalleryItemClick(event) {
+    if (event.target.nodeName !== 'IMG') {
+        return
+    };
+
+    event.preventDefault();
+
+    const optionsForInstace = {
+        onShow: () => {document.addEventListener('keydown', escPressListener)},
+        onClose: () => {document.removeEventListener('keydown', escPressListener)}
+    }
+
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    optionsForInstace);
+    
+    instance.show()
+
+    function escPressListener(e) {
+        if (e.code === 'Escape') {
+        instance.close()
+        }
+    }
+}
+
+
+function createMurkupFromObjArr(arr) {
+   return arr.map(el => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="large-image.jpg">
+    <img class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"/>
+    </a>
+    </div>`
+}).join("")
+}
+
+   
+
+
+// function onGalleryItemClick(event) {
+//     if (event.target.nodeName !== 'IMG') {
+//         return
+//     };
+
+//     event.preventDefault();
+//     const instance = basicLightbox.create(`
+//     <img src="${event.target.dataset.source}" width="800" height="600">
+//     `);
+//     instance.show()
+
+//     document.addEventListener('keydown', escPressListener)
+
+//     function escPressListener(e) {
+//         console.log(e);
+//     if (!document.querySelector('.basicLightbox')) {
+//         document.removeEventListener('keydown', escPressListener)
+//         }
+
+//     if (e.code === 'Escape') {
+//         instance.close()
+//         }
+//     }
+// }
+
+
+    
+    
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,19 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryMarkup = galleryItems.map(e => {
+    return `<a class="gallery__item" href="${e.original}">
+  <img class="gallery__image" src="${e.preview}" alt="${e.description}" />
+</a>`
+}).join('');
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = galleryMarkup;
+
+new SimpleLightbox('.gallery .gallery__item', {
+    captionsData: 'alt',
+    captionDelay: 250,
+})
+
